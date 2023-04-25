357d3d324037abbdb8245731fb2bb8b16c3fa242
joelofrese <joelofrese01@gmail.com>
2023-04-25 14:32:40 -0700

    hedgeball

---
+++ Ballmadillo/Assets/Scripts/RestartLevelWithButton.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using System. Collections;
+using System. Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevelwithButton : MonoBehaviour
+{
+    [SerializeField]
+    KeyCode keyRestart;
+    void Update()
+    {
+        if (Input.GetKey(keyRestart))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
