09673f450f2aa47bf7657e1e42461ac333dd9c6e
oliviapetrine <100885702+oliviapetrine@users.noreply.github.com>
2023-05-11 14:48:41 -0700

    work in prog

--- Ballmadillo/Assets/BackToMainMenu.cs
+++ Ballmadillo/Assets/BackToMainMenu.cs
@@ -5,6 +5,10 @@ public class BackToMainMenu : MonoBehaviour
 {
     public void OnClick()
     {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+
         SceneManager.LoadScene("Menu");
     }
 }
---
+++ Ballmadillo/Assets/ChooseMap.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource music;
+
+    public void Map1(){
+        music.Stop(); 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Map2(){
+        music.Stop(); 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+}
